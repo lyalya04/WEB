ab66d06e4da1c18e1a9b9fba657bd571f035f58a
Alevtina <alya@MacBook-Air--Alya.local>
2026-04-30 13:47:44 +0200

    break test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,4 @@
-<?php
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
+public function testTrueIsTrue()
 {
-   public function testTrueIsTrue()
-   {
-       $this->assertTrue(true);
-   }
+    $this->assertEquals(2, 1 + 2);
 }
